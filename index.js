eb6aecc24b1347f21ff58216e6b8629db0e9ef67
Jamila <jamnjeri@gmail.com>
2022-11-30 15:54:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(nameOfDrivers, name){
+  let checkDriver = nameOfDrivers.filter(item=> item.toUpperCase() === name.toUpperCase())
+  console.log (checkDriver)
+  return checkDriver
+}
+function fuzzyMatch(nameOfDrivers, letter){
+  let checkFirstLetter= nameOfDrivers.filter(item=> item.startsWith(letter));
+  console.log (checkFirstLetter)
+  return checkFirstLetter
+}
+function matchName(drivers, selection){
+  let matchName= drivers.filter(item=> item.name === selection)
+  console.log (matchName)
+  return matchName
+}
+
+
+findMatching(nameOfDrivers, 'Bobby');
+
+fuzzyMatch(nameOfDrivers, 'B');
+
+matchName(drivers, "Bobby");
+
+
+
+
+
